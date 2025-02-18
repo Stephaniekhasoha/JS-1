fe22f73f9c57675f50f08829ee82f73a48ed25c4
Stephaniekhasoha <khasohastephanie@gmail.com>
2025-02-19 00:40:40 +0300

    Worked on JS fundamentals

---
+++ script.js
@@ -0,0 +1,49 @@
+// This is an inline comment
+
+/* This is a multi-line comment */
+// Data Types (BUNNS)
+//Boolean is either true/false
+// declare variables using key words(var, let, const);
+let issLoggedIn = true;
+let stephIsAsleep = false;
+let hasBooked = false;
+let stephIsTall = false;
+console.log(stephIsAsleep);
+
+// Undefined 
+let firstName;
+// Let allows reassignment
+firstName = "Stephanie"
+const LASTNAME = "Khasoha"
+
+
+//Null
+let myName = null;
+let stephIsEating = null;
+
+
+//Numbers
+let age = 50; 
+let results = 90.4;
+let temperature = -30;
+
+// Strings
+const lastName = "Khasoha"
+let greetings = "HelloWorld"
+
+// Objects / Key-value
+
+let person = {
+    age: "50",
+    hobby: "sleep",
+    profession: "Developer"
+}
+
+// arrays
+let items = [50, "Stephanie",
+    person = {
+        age: "50",
+    hobby: "sleep",
+    profession: "Developer"
+    },true,false
+]

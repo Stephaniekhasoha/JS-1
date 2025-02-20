153c76ba1fd6941f25ef0f21a35b4f79767830ca
Stephaniekhasoha <khasohastephanie@gmail.com>
2025-02-20 22:51:25 +0300

    covered arrays

---
+++ arrays.js
@@ -0,0 +1,32 @@
+// arrays
+let studentGradeInMath = [95,88,44,91,76];
+// accessing arrays
+let firstGrade = studentGradeInMath[0];
+let secondGrade = studentGradeInMath[1];
+// length of arrays
+let lengthOfArray = studentGradeInMath.length;
+// console.log(lengthOfArray);
+// Accessing last element of array
+let lastGrade = studentGradeInMath[lengthOfArray-1];
+let sumOfGrades = studentGradeInMath[0]+studentGradeInMath[1];
+
+// Modifying arrays
+studentGradeInMath[2] = 100;
+
+//push(), pop(),unshift(),shift(),
+
+//push() add an array at the end
+studentGradeInMath.push(30);
+// unshift() add at the beginning
+studentGradeInMath.unshift(55);
+// pop() remove the array at the end
+studentGradeInMath.pop();
+// shift() remove the array at the beginning
+studentGradeInMath.shift();
+
+let softwareDevelopers = ["Dennis","Steph","Candacy"];
+let removeDeveloper = softwareDevelopers.pop();
+let removeNewDeveloper = softwareDevelopers.shift
+
+
+
---
+++ challenge.js
@@ -0,0 +1,12 @@
+let groceryList = ["milk","bread","eggs","butter","cheese"];
+// adding
+// groceryList.unshift("apples");
+// groceryList.push("bananas");
+
+// Removing
+// groceryList.splice(4,1);
+
+// slice(accessing)
+let breakFastFood = groceryList.slice(0,3);
+
+let fattyFoods = groceryList.slice(3);
